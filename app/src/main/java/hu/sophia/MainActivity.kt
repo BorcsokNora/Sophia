24b60cf11d69f67fded0e9ecdcc5091503be1e80
nora <nborcsok@dev-id.fr>
2021-12-23 20:16:13 +0100

    show random answer on button click

--- app/src/main/java/hu/sophia/MainActivity.kt
+++ app/src/main/java/hu/sophia/MainActivity.kt
@@ -7,26 +7,35 @@ import androidx.compose.ui.graphics.*
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color.Companion.Gray
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import hu.sophia.ui.theme.SophiaTheme
+import hu.sophia.ui.theme.themeColorBackground
+import hu.sophia.ui.theme.themeTextColor
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,13 +43,81 @@ class MainActivity : ComponentActivity() {
         setContent {
             SophiaTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    OptionsContainer()
+                Surface(color = themeColorBackground) {
+                    SimpleContent(
+                        modifier = Modifier.fillMaxSize()
+                    )
                 }
             }
         }
     }
 }
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun SimpleContent(
+    modifier: Modifier = Modifier,
+) {
+    var answer by remember { mutableStateOf("") }
+    var visible by remember { mutableStateOf(true) }
+
+    // todo: animate fade in of answer:
+    // https://developer.android.com/jetpack/compose/animation
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(Modifier.weight(1f))
+        AnimatedVisibility(
+            visible = visible,
+            enter = fadeIn(initialAlpha = 0f),
+            //exit = fadeOut()
+        ) {
+            Text(
+                text = answer,
+                modifier = Modifier.padding(horizontal = 16.dp),
+                textAlign = TextAlign.Center,
+                fontSize = 36.sp,
+                fontWeight = FontWeight.W500,
+                color = themeTextColor
+            )
+        }
+        Spacer(Modifier.weight(1f))
+        Button(onClick = {
+            visible = false
+            answer = getRandomAnswer()
+            visible = true
+        }) {
+            Text(
+                text = "Segíts dönteni",
+                fontSize = 24.sp
+            )
+        }
+        Spacer(Modifier.weight(1f))
+    }
+}
+
+fun getRandomAnswer(): String {
+    val options = listOf(
+        "Persze.",
+        "Dehogyis.",
+        "Talán...",
+        "Kérdezd meg holnap.",
+        "Erre Te is tudod a választ.",
+        "Ötletem sincs.",
+        "Bazsa erre mit mondana?",
+        "Déjá vu-m van...",
+        "Ezt te sem mondod komolyan.",
+        "Az élet nagy kérdései...",
+        "Ez ennél összetettebb...",
+        "Ne gondold túl.",
+        "Csak bártran!"
+    )
+    val index = Random.nextInt(0, options.size - 1)
+    return options[index]
+}
+
 
 @Composable
 fun OptionsContainer() {
@@ -49,7 +126,8 @@ fun OptionsContainer() {
         Box(
             Modifier
                 .height(200.dp)
-                .border(BorderStroke(2.dp, color = Gray), shape = RectangleShape))
+                .border(BorderStroke(2.dp, color = Gray), shape = RectangleShape)
+        )
         {
 
             OptionsListComposable()
@@ -60,7 +138,7 @@ fun OptionsContainer() {
 }
 
 @Composable
-fun ColorOverlay(){
+fun ColorOverlay() {
 
 }
 
@@ -83,9 +161,18 @@ fun OptionsListComposable() {
     }
 }
 
+
 @Preview(showBackground = true)
 @Composable
-fun OptionsContainerPreview(){
+fun SimpleContentPreview() {
+    SophiaTheme {
+        SimpleContent(Modifier.fillMaxSize())
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OptionsContainerPreview() {
     OptionsContainer()
 }
 
--- app/src/main/java/hu/sophia/ui/theme/Color.kt
+++ app/src/main/java/hu/sophia/ui/theme/Color.kt
@@ -2,6 +2,11 @@ package hu.sophia.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val themeColor = Color(0xFFC77C65)
+val themeColorLight = Color(0xFFF1AD98)
+val themeColorBackground = Color(0xFFFAEEEA)
+val themeTextColor = Color(0xFF5F5F5F)
+
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
